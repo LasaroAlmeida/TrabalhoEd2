f7e72a48b39581ec0ac7594569918458d19ccfa9
Lasaro Almeida <lasarodalmeida@gmail.com>
2020-10-14 14:25:08 -0300

    Primeiro Envio

---
+++ TrabalhoEd2_parte1/src/trabalhoed2_parte1/TrabalhoEd2_parte1.java
@@ -0,0 +1,16 @@
+package trabalhoed2_parte1;
+
+/**
+ *
+ * @author Lasaro Almeida
+ */
+public class TrabalhoEd2_parte1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
