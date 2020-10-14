8889cae3785fd60a7d91582218b22b88fc90971f
root <root@LAPTOP-HD1IPGB1.localdomain>
2020-10-14 17:38:55 -0300

    Segunda

---
+++ src/trabalhoed2_parte1/Registros.java
@@ -0,0 +1,13 @@
+package trabalhoed2_parte1;
+
+
+public class Registros {
+    
+    private String edition;
+    private int id;
+    private String isbn10;
+    private String isbn13;
+    private double rating_avg;
+    private int rating_count;
+    private String title;
+}
