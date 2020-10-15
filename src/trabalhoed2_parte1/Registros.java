4b0ea0eabe4f7c501b2864b21d5eac57c0b5149d
denermdn <denerloizz@gmail.com>
2020-10-15 19:38:12 -0300

    O problema foi resol

--- src/trabalhoed2_parte1/Registros.java
+++ src/trabalhoed2_parte1/Registros.java
@@ -1,13 +1,136 @@
 package trabalhoed2_parte1;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Registros {
     
+    private List<Integer> authors;
+    private int contauthors;
+    private List<Integer> bestsellersr;
+    private List<Integer> categories;
+    private int contategorias;
     private String edition;
-    private int id;
+    private BigInteger id;
     private String isbn10;
     private String isbn13;
     private double rating_avg;
     private int rating_count;
     private String title;
+    public Registros()
+    {
+        this.edition="";
+        contauthors=0;
+        this.authors=new ArrayList<Integer>();
+        this.bestsellersr=new ArrayList<Integer>();
+        contategorias=0;
+        this.categories=new ArrayList<Integer>();
+    }
+    public void setAuthors(String str)
+    {
+        String aux="";
+        int auxx=1;
+        if(str=="")
+        {
+            authors.add(-1);
+            return;
+        }
+        while(str.charAt(auxx)!=']')
+        {
+            
+        while(str.charAt(auxx)!=',' && str.charAt(auxx)!=']')
+        {
+            aux=aux+str.charAt(auxx);
+            auxx++;
+        }
+        authors.add(Integer.parseInt(aux));
+            System.out.println(authors.get(contauthors));
+            contauthors++;
+        if(str.charAt(auxx)==',')
+            auxx=auxx+2;
+        aux="";
+        }
+        return;
+    }
+
+    public List<Integer> getAuthors() {
+        return authors;
+    }
+
+    public List<Integer> getBestsellersr() {
+        return bestsellersr;
+    }
+
+    public void setBestsellersr(String str) {
+        int auxx=str.length(),contt=0,guarda;
+        System.out.println(auxx);
+        String aux="";
+        if(str=="")
+        {
+            bestsellersr.add(-1);
+            return;
+        }
+        while(contt<auxx-1)
+        {
+        while(str.charAt(contt)!=',' && contt<auxx)
+        {
+            System.out.println(contt);
+            aux=aux+str.charAt(contt);
+            System.out.println(aux);
+            contt++;
+            if(contt>auxx-1)
+                break;
+        }
+//        guarda=(Integer.parseInt(aux));
+            System.out.println(aux);
+//        bestsellersr.add(guarda);
+        bestsellersr.add(Integer.parseInt(aux));
+            System.out.println("Impresso");
+        aux="";
+        }
+        
+    }
+    public void setcategories(String str)
+    {
+        String aux="";
+        int auxx=1;
+        if(str=="")
+        {
+            categories.add(-1);
+            return;
+        }
+        while(str.charAt(auxx)!=']')
+        {
+            
+        while(str.charAt(auxx)!=',' && str.charAt(auxx)!=']')
+        {
+            aux=aux+str.charAt(auxx);
+            auxx++;
+            System.out.println(aux);
+        }
+        categories.add(Integer.parseInt(aux));
+            System.out.println(categories.get(contategorias));
+            contategorias++;
+        if(str.charAt(auxx)==',')
+            auxx=auxx+2;
+        aux="";
+        }
+        return;
+    }
+    
+    public void setedition(String str)
+    {
+        this.edition=str;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+    public void setId(String str)
+    {
+        id=new BigInteger(str);
+        System.out.println(this.id);
+    }
 }
--- src/trabalhoed2_parte1/TrabalhoEd2_parte1.java
+++ src/trabalhoed2_parte1/TrabalhoEd2_parte1.java
@@ -2,25 +2,88 @@ package trabalhoed2_parte1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TrabalhoEd2_parte1 {
 
     public static void main(String[] args) {
         System.out.println("Alteração do Dener KEKW");
-        File input_file=new File(args[0]);
-        try{
-            Scanner leitor= new Scanner(input_file);
-            String linha;
-            while(leitor.hasNext()){
-                linha=leitor.nextLine();
-                System.out.println(linha);
+        File input_file = new File(args[0]);
+        String separa = "", junta, linha;
+        List<Registros> a= new ArrayList<Registros>();
+        Registros b=new Registros();
+        List<Integer> n=new ArrayList<Integer>();
+        int cont1 = 1, cont2 = 0, cont3 = 0, aux = 0,auxs=0;
+        try {
+            Scanner leitor = new Scanner(input_file);
+            while (leitor.hasNext()) {
+                linha = leitor.nextLine();
+                aux = linha.length();
+                cont1 = 1;
+                cont2 = 0;
+                separa = linha;
+                for (int i = 0; i < 10 && cont1 < aux; i++) {
+                    junta = "";
+                    while (separa.charAt(cont1) == '"' || separa.charAt(cont1) == ',') {
+                        if (separa.charAt(cont1) == '"' && separa.charAt(cont1 + 1) == '"') {
+                            cont1++;
+                            break;
+                        }
+                        cont1++;
+                    }
+                    while (separa.charAt(cont1) != '"') {
+                        junta = junta + separa.charAt(cont1);
+                        cont1++;
+                    }
+                    // System.out.println(junta);
+                    switch (cont2) {
+                        case 0:
+                            //if(cont1<aux-1)
+                            b.setAuthors(junta);
+                            break;
+                        case 1:
+                            System.out.println(junta);
+                            b.setBestsellersr(junta);
+                            break;
+                        case 2:
+                            System.out.println(junta);
+                            b.setcategories(junta);
+                            break;
+                        case 3:
+                            System.out.println(junta);
+                            b.setedition(junta);
+                            break;
+                        case 4:
+                            System.out.println(junta);
+//                            BigInteger decima=new BigInteger(junta);
+//                            System.out.println(decima);
+//                            System.out.println("Sua");
+                            b.setId(junta);
+                        default:
+                        System.out.println("trabalhoed2_parte1.TrabalhoEd2_parte1.main()");
+                        //System.out.println(cont2);
+                    }
+                    // System.out.println(cont2);
+                    cont2 += 1;
+                }
+                a.add(b);
+//                n=a.get(cont3).getAuthors();
+//                for(int i=0;i<n.size();i++)
+//                {
+//                    System.out.println(n.get(i));
+//                }
+                cont3++;
+                b=new Registros();
+                //              System.out.println(linha);
             }
-            
-        }catch(FileNotFoundException e){
+
+        } catch (FileNotFoundException e) {
             System.out.println("Erro ao abrir arquivo");
         }
-        System.out.println("trabalhoed2_parte1.TrabalhoEd2_parte1.main()");
+//        System.out.println("trabalhoed2_parte1.TrabalhoEd2_parte1.main()");
     }
-    
+
 }
