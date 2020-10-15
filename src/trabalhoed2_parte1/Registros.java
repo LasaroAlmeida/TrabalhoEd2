23497d10fe0a96bdffc16f00d0edee272807df12
root <root@LAPTOP-HD1IPGB1.localdomain>
2020-10-15 19:25:43 -0300

    modifiquei

--- src/trabalhoed2_parte1/Registros.java
+++ src/trabalhoed2_parte1/Registros.java
@@ -1,13 +1,80 @@
 package trabalhoed2_parte1;
 
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Registros {
     
+    private List<Integer> authors;
     private String edition;
-    private int id;
+    private BigInteger id;
     private String isbn10;
     private String isbn13;
     private double rating_avg;
     private int rating_count;
+
+    public double getRating_avg() {
+        return rating_avg;
+    }
+
+    public String getIsbn13() {
+        return isbn13;
+    }
     private String title;
+    public Registros()
+    {
+        this.edition="";
+        this.authors=new ArrayList<Integer>();
+    }
+
+    public void setAuthors(List<Integer> authors) {
+        this.authors = authors;
+    }
+
+    public int getRating_count() {
+        return rating_count;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    
+    public void setIsbn10(String isbn10) {
+        this.isbn10 = isbn10;
+    }
+
+    public void setIsbn13(String isbn13) {
+        this.isbn13 = isbn13;
+    }
+
+    public void setRating_avg(double rating_avg) {
+        this.rating_avg = rating_avg;
+    }
+
+    public void setRating_count(int rating_count) {
+        this.rating_count = rating_count;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+   
+
+    public String getEdition() {
+        return edition;
+    }
+
+ 
 }
--- src/trabalhoed2_parte1/TrabalhoEd2_parte1.java
+++ src/trabalhoed2_parte1/TrabalhoEd2_parte1.java
@@ -2,25 +2,88 @@ package trabalhoed2_parte1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TrabalhoEd2_parte1 {
 
     public static void main(String[] args) {
         System.out.println("Alteração do Dener KEKW");
-        File input_file=new File(args[0]);
-        try{
-            Scanner leitor= new Scanner(input_file);
-            String linha;
-            while(leitor.hasNext()){
-                linha=leitor.nextLine();
-                System.out.println(linha);
+        File input_file = new File(args[0]);
+        String separa = "", junta, linha;
+        List<Registros> a = new ArrayList<Registros>();
+        Registros b = new Registros();
+        int cont1 = 1, cont2 = 0, cont3 = 0, aux = 0;
+        try {
+            Scanner leitor = new Scanner(input_file);
+            while (leitor.hasNext()) {
+                linha = leitor.nextLine();
+                aux = linha.length();
+                cont1 = 1;
+                cont2 = 0;
+                separa = linha;
+                for (int i = 0; i < 10 && cont1 < aux; i++) {
+                    junta = "";
+                    while (separa.charAt(cont1) == '"' || separa.charAt(cont1) == ',') {
+                        if (separa.charAt(cont1) == '"' && separa.charAt(cont1 + 1) == '"') {
+                            cont1++;
+                            break;
+                        }
+                        cont1++;
+                    }
+                    while (separa.charAt(cont1) != '"') {
+                        junta = junta + separa.charAt(cont1);
+                        cont1++;
+                    }
+                    // System.out.println(junta);
+                    switch (cont2) {
+                        case 0:
+                           // b.setAuthors(junta);
+                            break;
+                        case 4:
+                            BigInteger decima=new BigInteger(junta);
+                            b.setId(decima);
+                        case 5:
+                            b.setIsbn10(junta);
+                            break;
+                        case 6:
+                            b.setIsbn13(junta);
+                            break;
+                        case 7:
+                            if (!junta.equals(",") && !junta.equals("")) {
+                                b.setRating_avg(Double.parseDouble(junta));
+                            } else {
+                                b.setRating_avg(-1.0);
+                            }
+
+                            break;
+                        case 8:
+                             if (!junta.equals(",") && !junta.equals("")) {
+                                 b.setRating_count(Integer.parseInt(junta));
+                             }
+                            break;
+                        case 9:
+                            b.setTitle(junta);
+                            break;
+
+                        default:
+                            System.out.println("Posicao invalida");
+
+                    }
+                    cont2 += 1;
+                }
+                System.out.println(b.getId());
+                a.add(b);
+                System.out.println(a.get(1).getId());
+                cont3++;
             }
-            
-        }catch(FileNotFoundException e){
+
+        } catch (FileNotFoundException e) {
             System.out.println("Erro ao abrir arquivo");
         }
-        System.out.println("trabalhoed2_parte1.TrabalhoEd2_parte1.main()");
+//        );
     }
-    
+
 }
